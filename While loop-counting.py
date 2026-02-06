445340731f47b3be1e9469f6a6c14effeb31d5cd
tinaphun9 <tinaphun9@gmail.com>
2026-02-05 20:16:10 -0800

    Create While loop-counting.py

---
+++ While loop-counting.py
@@ -0,0 +1,17 @@
+'''
+# Add a Comment Here to describe/explain what you are doing
+
+# This program uses a while loop to count from 0 up to (but not including) 10
+
+def main():
+    print("Starting While Loop - Print Count Variable")
+
+    number = 0  #to start counting from 0
+
+    while number < 10:#to count from number(0) to 9
+        print(number)
+        number += 1   #to increase by 1 each time
+
+    print("Ending While Loop")
+
+main()
